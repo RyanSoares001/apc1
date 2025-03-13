6068ca2722083bd357087c592d332f6e6fa781ef
RyanSoares001 <ryandeus10@gmail.com>
2025-03-12 21:50:37 -0300

    codigos da aula 2

---
+++ aulas/aula02/imprimir.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){
+    printf("boa noite!\n");
+    printf ("\tEstamos aprendendo em C!");
+    return 0;
+}
---
+++ aulas/aula02/imprimir_caracter.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+ 
+    int main(){
+        printf("%c\n", 'A');
+
+        printf("Ola %s\n", "Rods");
+        return 0;
+    }
